bf8d114b0d288cebdeacc4d89e887c47691a9d95
Muhammmet Emin BAYTAR <44005768+muhammeteminbaytar@users.noreply.github.com>
2020-08-19 16:39:08 +0300

    Add files via upload

---
+++ calculator.dart
@@ -0,0 +1,29 @@
+import 'dart:io' show stdin;
+
+main(List<String> args) {
+  int n1, n2;
+  String islem;
+  double sonuc;
+
+  print('Birinci sayıyı girin.');
+  n1 = int.parse(stdin.readLineSync());
+  print('İkinci sayıyı girin.');
+  n2 = int.parse(stdin.readLineSync());
+  print('yapmak istediğiniz işlemi seçiniz \n (+,-,*,/)');
+  islem = stdin.readLineSync();
+  if (islem == '+') {
+    sonuc = (n1 + n2).toDouble();
+    print("Sonuç :$sonuc");
+  } else if (islem == '-') {
+    sonuc = (n1 - n2).toDouble();
+    print("Sonuç :$sonuc");
+  } else if (islem == 'x') {
+    sonuc = (n1 * n2).toDouble();
+    print("Sonuç :$sonuc");
+  } else if (islem == '*') {
+    sonuc = n1 / n2;
+    print("Sonuç :$sonuc");
+  } else {
+    print("Tanımsız işlem yaptınız.");
+  }
+}
